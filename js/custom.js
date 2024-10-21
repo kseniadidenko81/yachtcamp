ee4b31deaec7e92d4eaac493103fd3b73852584b
kseniadidenko81 <57663840+kseniadidenko81@users.noreply.github.com>
2024-10-21 23:53:10 +0300

    Update custom.js34

--- js/custom.js
+++ js/custom.js
@@ -66,7 +66,7 @@ $(function () {
   });
 
   // ANCHOR
- document.querySelectorAll(".btn").forEach((button) => {
+  document.querySelectorAll(".btn").forEach((button) => {
     button.addEventListener("click", function () {
       const targetSection = document.querySelector(".bg-form.text-white");
       if (targetSection) {
@@ -96,114 +96,48 @@ $(function () {
   });
 
   // GALLERY + CITATION
+});
 
-  function createCarousel(carouselSelector) {
-    var $carousel = $(carouselSelector);
-    var items = [];
-    var startItem = 1;
-    var position = 0;
-    var itemCount = $carousel.find("li").length;
-    var resetCount = itemCount;
+var swiper1 = new Swiper(".swiper1", {
+  autoHeight: true,
+  slidesPerView: 1,
+  loop: true,
+  loopedSlides: 3,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: -100,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 800,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper1-button-next",
+    prevEl: ".swiper1-button-prev",
+  },
+});
 
-    $carousel.find("li").each(function (index) {
-      items[index] = $(this).text();
-    });
-
-    function swap(action) {
-      var direction = action;
-
-      if (direction == "counter-clockwise") {
-        var leftItem = $carousel.find(".left-pos").attr("id") - 1;
-        if (leftItem == 0) {
-          leftItem = itemCount;
-        }
-
-        $carousel
-          .find(".right-pos")
-          .removeClass("right-pos")
-          .addClass("back-pos");
-        $carousel
-          .find(".main-pos")
-          .removeClass("main-pos")
-          .addClass("right-pos");
-        $carousel
-          .find(".left-pos")
-          .removeClass("left-pos")
-          .addClass("main-pos");
-        $carousel
-          .find("#" + leftItem)
-          .removeClass("back-pos")
-          .addClass("left-pos");
-
-        startItem--;
-        if (startItem < 1) {
-          startItem = itemCount;
-        }
-      }
-
-      if (direction == "clockwise" || direction == "" || direction == null) {
-        function pos(positionValue) {
-          if (positionValue != "leftposition") {
-            position++;
-
-            if (startItem + position > resetCount) {
-              position = 1 - startItem;
-            }
-          }
-
-          if (positionValue == "leftposition") {
-            position = startItem - 1;
-
-            if (position < 1) {
-              position = itemCount;
-            }
-          }
-
-          return position;
-        }
-
-        $carousel
-          .find("#" + startItem)
-          .removeClass("main-pos")
-          .addClass("left-pos");
-        $carousel
-          .find("#" + (startItem + pos()))
-          .removeClass("right-pos")
-          .addClass("main-pos");
-        $carousel
-          .find("#" + (startItem + pos()))
-          .removeClass("back-pos")
-          .addClass("right-pos");
-        $carousel
-          .find("#" + pos("leftposition"))
-          .removeClass("left-pos")
-          .addClass("back-pos");
-
-        startItem++;
-        position = 0;
-        if (startItem > itemCount) {
-          startItem = 1;
-        }
-      }
-    }
-
-    $carousel.find(".next").click(function () {
-      swap("clockwise");
-    });
-
-    $carousel.find(".prev").click(function () {
-      swap("counter-clockwise");
-    });
-
-    $carousel.find(".items").click(function () {
-      if ($(this).hasClass("left-pos")) {
-        swap("counter-clockwise");
-      } else {
-        swap("clockwise");
-      }
-    });
-  }
-
-  createCarousel("#carousel1");
-  createCarousel("#carousel2");
+var swiper2 = new Swiper(".swiper2", {
+  autoHeight: true,
+  slidesPerView: 1,
+  loop: true,
+  loopedSlides: 3,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: -100,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 800,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper2-button-next",
+    prevEl: ".swiper2-button-prev",
+  },
 });
