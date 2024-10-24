c8d2d6fd61eba1d138e96dcb1e07e79e062d0c34
kseniadidenko81 <57663840+kseniadidenko81@users.noreply.github.com>
2024-10-24 09:46:43 +0300

    Update privacy-terms.jsde

--- js/privacy-terms.js
+++ js/privacy-terms.js
@@ -41,7 +41,7 @@ $(function () {
   $(function () {
     function adjustContentPadding() {
       var navbarHeight = $("#navbar").outerHeight();
-      var extraPadding = 0;
+      var extraPadding = 12;
       $("#content").css("padding-top", navbarHeight + extraPadding + "px");
     }
 
