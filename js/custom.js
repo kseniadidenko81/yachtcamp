6fcf1f85b0c7b3421072d22dbda0ed06d06cc239
kseniadidenko81 <57663840+kseniadidenko81@users.noreply.github.com>
2024-11-07 19:54:43 +0200

    Update custom.jsdfgh

--- js/custom.js
+++ js/custom.js
@@ -165,6 +165,52 @@ $(function () {
 });
 
 // READ MORE/LESS TEXT
+// $(function () {
+//   $(document).ready(function () {
+//     let swiper = new Swiper(".instrSwiper", {
+//       slidesPerView: 1,
+//       loop: true,
+//       speed: 350,
+//       spaceBetween: 20,
+//       centeredSlides: true,
+//       autoHeight: true,
+
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+
+//     $(".read-more").on("click", function () {
+//       let $target = $(this)
+//         .siblings(".read-more-wrap")
+//         .find(".read-more-target");
+
+//       $(".read-less").remove();
+//       $(".read-more").show();
+
+//       $target.stop(true).slideDown(500, "swing", () => {
+//         swiper.update();
+//       });
+
+//       $(this).hide();
+//       $(this).after('<span class="read-less">Read Less</span>');
+//     });
+
+//     $(document).on("click", ".read-less", function () {
+//       let $target = $(this)
+//         .siblings(".read-more-wrap")
+//         .find(".read-more-target");
+
+//       $target.stop(true).slideUp(500, "swing", () => {
+//         swiper.update();
+//       });
+
+//       $(".read-more").show();
+//       $(this).remove();
+//     });
+//   });
+// });
 $(function () {
   $(document).ready(function () {
     let swiper = new Swiper(".instrSwiper", {
@@ -173,6 +219,7 @@ $(function () {
       speed: 350,
       spaceBetween: 20,
       centeredSlides: true,
+      autoHeight: true,
 
       navigation: {
         nextEl: ".swiper-button-next",
@@ -185,8 +232,12 @@ $(function () {
         .siblings(".read-more-wrap")
         .find(".read-more-target");
 
+      $(".read-less").remove();
+      $(".read-more").show();
+
       $target.stop(true).slideDown(500, "swing", () => {
         swiper.update();
+        $target.addClass("slide-down"); // Добавляем класс для плавного появления
       });
 
       $(this).hide();
@@ -200,6 +251,7 @@ $(function () {
 
       $target.stop(true).slideUp(500, "swing", () => {
         swiper.update();
+        $target.removeClass("slide-down");
       });
 
       $(".read-more").show();
